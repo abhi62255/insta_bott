9ca774f4e605b5119d3f2f858ded605a8bd32541
abhi62255 <abhi62255@gmail.com>
2017-07-05 10:58:15 +0530

    Initialize main.py and constants.py files

---
+++ constants.py
@@ -0,0 +1,6 @@
+# File to introduce constants
+import requests
+
+APP_ACCESS_TOKEN='1690315985.1677ed0.b2624941c475410ba75f7f72e214fcbe'
+BASE_URL='https://api.instagram.com/v1/'
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from self_info import self_info
+from get_user_id import get_user_id
+
+self_info()
